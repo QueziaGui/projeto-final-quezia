fe0c462f9d51695fab3d23a26e1c815fb2795abb
Quezia Guilhermina Guilhermina <queziagds@gmail.com>
2021-12-16 15:13:27 -0300

    Ajustando responsividade

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,68 +7,74 @@ import Footer  from '../../Componentes/Footer/Footer';
 
 
 const Home = () => {
-    return(
+   return(
       <>
         <Menu />
-      <div className='principal'> 
-        <h1 className='titulo'>Proteção Animal!</h1>
+        <div className='principal'> 
+              <h1 className='titulo'>Proteção Animal!</h1>
         
-        <div>
-          <img src={cao} className="imagelogo" alt="logo"/>
-        </div>
+              <div>
+               <img src={cao} className="imagelogo" alt="logo"/>
+              </div>
         
-        <div className="texto">
-          <h2>Legislação</h2>
-          <p>
-            Há um árduo trabalho por parte da sociedade e autoridades em conscientizar as pessoas sobre 
-            o tratamento adequado dos animais.
-             A agência de investigação dos Estados Unidos FBI tem realizado estudos sobre o perfil de 
-             pessoas que maltratam animais com potencial desenvolvimento de se tornar um psicopata. 
-          </p>
-          <p>De acordo com psiquiatras as pessoas que maltratam animais ignoram que estes seres vivos sentem dores,
-             são apegadas as pessoas que lhe cercam e que necessitam de cuidados.</p>
+              <div className="texto">
+                <h2>Legislação</h2>
+                <p>
+                  Há um árduo trabalho por parte da sociedade e autoridades em conscientizar as pessoas sobre 
+                  o tratamento adequado dos animais.
+                  A agência de investigação dos Estados Unidos FBI tem realizado estudos sobre o perfil de 
+                  pessoas que maltratam animais com potencial desenvolvimento de se tornar um psicopata. 
+                </p>
+                <p>De acordo com psiquiatras as pessoas que maltratam animais ignoram que estes seres vivos sentem dores,
+                  são apegadas as pessoas que lhe cercam e que necessitam de cuidados.
+                </p>
 
-          <p>Partindo deste pressuposto observamos que é necessário estar atento a comportamento das pessoas quando maltratam os animais.
-             As autoridades devem ser acionadas imediatamente. Fonte pesquisa: " https://radiopeaobrasil.com.br/psiquiatra-forense-aponta-que-maltratar-animais-e-indicio-de-psicopatia/"
-          </p>
+                <p>Partindo deste pressuposto observamos que é necessário estar atento a comportamento das pessoas quando maltratam os animais.
+                    As autoridades devem ser acionadas imediatamente. Fonte pesquisa: " https://radiopeaobrasil.com.br/psiquiatra-forense-aponta-que-maltratar-animais-e-indicio-de-psicopatia/"
+                </p>
+              </div>
 
           
           
-          <ul className="declaracao"> Leia também sobre a Declaração Universal dos Direitos dos Animais – Unesco 1978
-          Algumas ações consideradas maus-tratos:
+            <ul className="declaracao">
+                <p> Leia também sobre a Declaração Universal dos Direitos dos Animais – Unesco 1978
+                    Algumas ações consideradas maus-tratos:
+                </p>
 
-          <li> 
-          •	não dar água e comida diariamente;
-          •	manter preso em corrente;
-          •	manter em local sujo e pequeno demais para que o animal possa andar ou correr;
-          •	deixar sem ventilação ou luz solar e desprotegido do vento, sol e chuva;
-          •	negar assistência veterinária a animal doente ou ferido;
-          •	obrigar a trabalho excessivo ou superior à sua força;
-          •	abandonar;
-          •	ferir;
-          •	envenenar;
-          •	utilizar para rinha, farra-do-boi, etc,;
-          •	vivissecção;
-          •	caça;
-          </li>
-          </ul>
+            
+                <li>•	não dar água e comida diariamente;</li>
+            
+                <li>•	manter preso em corrente;</li>
+            
+                <li>•	manter em local sujo e pequeno demais para que o animal possa andar ou correr;</li> 
+                <li>•	deixar sem ventilação ou luz solar e desprotegido do vento, sol e chuva;</li>
+                <li>•negar assistência veterinária a animal doente ou ferido;</li>
+                <li>•obrigar a trabalho excessivo ou superior à sua força;</li>
+                <li>•abandonar;</li>
+                <li>•ferir;</li>
+                <li>•envenenar;</li>
+                <li>•utilizar para rinha, farra-do-boi, etc,;</li>
+                <li>•vivissecção</li>
+                <li>•caça</li>
 
-        </div>
-        <div className= "saibaMais">
-          <button href= "https://radiopeaobrasil.com.br/psiquiatra-forense-aponta-que-maltratar-animais-e-indicio-de-psicopatia/">
-            Saiba Mais
-          </button>
+            </ul>
 
+          
+                <div className="button">
+                    <a href= "https://radiopeaobrasil.com.br/psiquiatra-forense-aponta-que-maltratar-animais-e-indicio-de-psicopatia/">
+                      <button>Saiba Mais</button>
+                    </a>
+                </div>
+            <div className='cartao'>
+                <h2>Cartão pet download</h2>
+                <a href="http://www.rganimal.com.br/RG/editar-carteira-azul.php" download="newfilename">Download do pdf</a>
+            </div>
+              
+            <Footer />
         </div>
-        <div className='cartao'>
-          <h2>Cartão pet download</h2>
-          <a href="http://www.rganimal.com.br/RG/editar-carteira-azul.php" download="newfilename">Download do pdf</a>
-        </div>
-        <Footer />
-      </div>
         
-      </>
+      </> 
     )
-  }
-  
+
+}
   export default Home
